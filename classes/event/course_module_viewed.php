3103f33360fb16d1f4f92569ec824a8dee470c78
JohnnyLinares <j.linares@asesoriamadeeasy.com>
2026-01-22 01:37:42 -0500

    Capsula v1.3

---
+++ classes/event/course_module_viewed.php
@@ -0,0 +1,17 @@
+<?php
+namespace mod_capsula\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+class course_module_viewed extends \core\event\course_module_viewed {
+
+    protected function init() {
+        $this->data['objecttable'] = 'capsula';
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+    }
+
+    public static function get_objectid_mapping() {
+        return array('db' => 'capsula', 'restore' => 'capsula');
+    }
+}
